6a4a65781bce2eee201de8a0261d3a7fca1608ac
Edgar Cebrero <edgaralan.cl@gmail.com>
2016-05-22 01:17:19 -0800

    Clases básico

---
+++ Proyecto_ISW/Proyecto_ISW/Clases/Alumno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_ISW
+{
+    class Alumno : Usuario
+    {
+        private string nombre { get; set; }
+        private string ap_paterno { get; set; }
+        private string ap_materno { get; set; }
+        private string idUsuario { get; set; }
+        private int matricula { get; set; }
+        private string unidadAcademica { get; set; }
+        private string correoAlt { get; set; }
+        private int horasPrimEtapa { get; set; }
+        private int horasSegEtapa { get; set; }
+        private DateTime fechaTallerPrimEtapa { get; set; }
+        private DateTime fechaTallerSegEtapa { get; set; }
+        private DateTime fechaAcPrimEtapa { get; set; }
+        private DateTime fechaAcSegEtapa { get; set; }
+
+    }
+}
---
+++ Proyecto_ISW/Proyecto_ISW/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_ISW
+{
+    abstract class Usuario 
+    {
+        private string nombre { get; set; }
+        private string pwd { get; set; }
+
+        public Usuario()
+        {
+
+        }
+
+    }
+}
