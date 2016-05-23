a55dc63791699f765f8522c3768d0273d04f08c4
Edgar Cebrero <edgaralan.cl@gmail.com>
2016-05-22 18:53:30 -0800

    Alumno branch test

--- ISW.SUSS.BLL/Modelos/Alumno.cs
+++ ISW.SUSS.BLL/Modelos/Alumno.cs
@@ -9,24 +9,27 @@ namespace Clases.Modelos
 {
     public class Alumno 
     {
-        private string nombre { get; set; }
-        private string ap_paterno { get; set; }
-        private string ap_materno { get; set; }
-        private string idUsuario { get; set; }
-        private int matricula { get; set; }
-        private string unidadAcademica { get; set; }
-        private string correoAlt { get; set; }
-        private int horasPrimEtapa { get; set; }
-        private int horasSegEtapa { get; set; }
-        private DateTime fechaTallerPrimEtapa { get; set; }
-        private DateTime fechaTallerSegEtapa { get; set; }
-        private DateTime fechaAcPrimEtapa { get; set; }
-        private DateTime fechaAcSegEtapa { get; set; }
+        private string nombre /*{ get; set; }*/;
+        public string ap_paterno { get; set; }
+        public string ap_materno { get; set; }
+        private string idUsuario /*{ get; set; }*/;
+        private int matricula /*{ get; set; }*/;
+        private string unidadAcademica /*{ get; set; }*/;
+        public string correoAlt { get; set; }
+        public int horasPrimEtapa { get; set; }
+        public int horasSegEtapa { get; set; }
+        public DateTime fechaTallerPrimEtapa { get; set; }
+        public DateTime fechaTallerSegEtapa { get; set; }
+        public DateTime fechaAcPrimEtapa { get; set; }
+        public DateTime fechaAcSegEtapa { get; set; }
         private ProgramaSS[] programas { get; set; }
 
-        public Alumno()
+        public Alumno(string nombre, int matricula, string unidadAc, string idUsuario)
         {
-
+            this.nombre = nombre;
+            this.matricula = matricula;
+            this.unidadAcademica = unidadAc;
+            this.idUsuario = idUsuario;
         }
 
     }
